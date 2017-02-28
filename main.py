8c99d8b45936c93ab63085ba879dc9207ed0c00f
Maxwell Nderitu <nderitumaxwell@gmail.com>
2017-02-28 20:47:09 +0300

    Adding support for computation using standard input

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import math
+# import sys
 
 
-# Function that enforces each line list within the entire list should have at less 3 values
+# Function that enforces each line list within the entire list should have at least 3 values
+# This handles situations that data is not present such as chemistry marks are not present
 def format_all_lines(lines_list):
     for i in range(len(lines_list)):
         assert type(lines_list[i]) is list
         # Check if the single line list has less than 3 values
         if len(lines_list[i]) < 3:
-            # Add zero where an element or elements is missing
+            # Add zero where an element or elements is/are missing
             for _ in range(3 - len(lines_list[i])):
                 lines_list[i].append("0")
 
@@ -50,37 +52,36 @@ def pearson_coefficient(x, y):
     upper_side = (length * sum_xy) - (sum_x * sum_y)
     lower_side = ((length * sum_pow_x) - pow(sum_x, 2)) * ((length * sum_pow_y) - pow(sum_y, 2))
 
+    # Account for any zero division errors
+    assert upper_side != 0
+    assert lower_side != 0
+
     return round(upper_side / math.sqrt(lower_side), 2)
 
-# Sample array that will be created
-# physics = [0, 76, 76, 95, 96, 79, 74, 97, 97, 90, 90, 78, 91, 76, 90, 95, 95, 75, 100, 87, 90]
-# chemistry = [0, 72, 67, 92, 95, 59, 58, 95, 94, 84, 83, 70, 79, 67, 73, 87, 86, 63, 92, 80, 76]
-# maths = [20, 73, 48, 95, 95, 33, 47, 98, 91, 95, 93, 70, 85, 33, 47, 95, 84, 43, 95, 54, 72]
-
 # Get the data from the input
+
+# Uncomment this if you are running from hacker rank / Reading from input
+# data = sys.stdin.readlines()
+
+# Comment this if you are running from hacker rank / Reading from the data.dat file
 with open("data.dat") as data:
     # Loop to get the data
     lines = [line.split() for line in data]
-    # Remove empty lists
-    lines = filter(None, lines)
 
-# Log
-print "The lines list is: ", lines
+# Removing the first line that is the number of students
+lines.pop(0)
 
-# Make sure the a single line has no less than 3 values
+# Make sure the a single line has no less than 3 values. i.e maths, chemistry, physics
 lines = format_all_lines(lines)
-
-# Log
-print "The new lines list is: ", lines
 
 # Get the first 3 rows required
 maths = [handle_value(row[0]) for row in lines]
-chemistry = [handle_value(row[1]) for row in lines]
-physics = [handle_value(row[2]) for row in lines]
+physics = [handle_value(row[1]) for row in lines]
+chemistry = [handle_value(row[2]) for row in lines]
 
-print "Maths list is: ", maths, "\n Chemistry list is: ", chemistry,\
-    "\n Physics list is: ", physics, "\n"
+# print("Maths list is: ", maths, "\n Chemistry list is: ", chemistry,
+#       "\n Physics list is: ", physics, "\n")
 
-print pearson_coefficient(maths, physics)
-print pearson_coefficient(physics, chemistry)
-print pearson_coefficient(chemistry, maths)
+print(pearson_coefficient(maths, physics))
+print(pearson_coefficient(physics, chemistry))
+print(pearson_coefficient(chemistry, maths))
